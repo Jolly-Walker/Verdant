59215d35ab535a8d5f929b62a057571c2dfdc22c
Jolly Walker <wz.lim.00@gmail.com>
2026-04-03 12:16:40 +0800

    chore: config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "pino-pretty": false,
+      "lokijs": false,
+      "encoding": false,
+      "net": false,
+      "tls": false,
+      "@react-native-async-storage/async-storage": false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
